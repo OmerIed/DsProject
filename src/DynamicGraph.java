cf38752f488c037bc753f10a96732498c570d497
Ben Van Oost <ben.van-oost@campus.technion.ac.il>
2021-12-11 18:58:35 +0200

    delete edge

--- src/DynamicGraph.java
+++ src/DynamicGraph.java
@@ -26,11 +26,56 @@ public class DynamicGraph {
 
     }
     public void deleteEdge(GraphEdge edge) {
-        edgeList.remove(edgeList.get);
-        edge.get
+        if (edge == edge.getTo().getInFirst()) {
+            if (edge.getToNext() != null) {
+                edge.getTo().setInFirst(edge.getToNext());
+                edge.getToNext().setToPrev(null);
+            } else {
+                edge.getTo().setInFirst(null);
+                edge.getTo().setInLast(null);
+            }
+        } else if (edge == edge.getTo().getInLast()) {
+            if (edge.getToPrev() != null) {
+                edge.getTo().setInLast(edge.getToPrev());
+                edge.getToPrev().setToNext(null);
+            } else {
+                edge.getTo().setInLast(null);
+                edge.getTo().setInFirst(null);
+            }
+        } else {
+            edge.getToPrev().setToNext(edge.getToNext());
+            edge.getToNext().setToPrev(edge.getToPrev());
+        }
 
-
+        if (edge == edge.getFrom().getOutFirst()) {
+            if (edge.getFromNext() != null) {
+                edge.getFrom().setOutFirst(edge.getFromNext());
+                edge.getFromNext().setFromPrev(null);
+            } else {
+                edge.getFrom().setOutFirst(null);
+                edge.getFrom().setOutLast(null);
+            }
+        } else if (edge == edge.getFrom().getOutLast()) {
+            if (edge.getFromPrev() != null) {
+                edge.getFrom().setOutLast(edge.getFromPrev());
+                edge.getFromPrev().setFromNext(null);
+            } else {
+                edge.getFrom().setOutLast(null);
+                edge.getFrom().setOutFirst(null);
+            }
+        } else {
+            edge.getFromPrev().setFromNext(edge.getFromNext());
+            edge.getFromNext().setFromPrev(edge.getFromPrev());
+        }
+        edge.setTo(null);
+        edge.setToNext(null);
+        edge.setFromPrev(null);
+        edge.setFromNext(null);
+        edge.setFromPrev(null);
+        edge.setFrom(null);
     }
+
+
     public  RootedTree scc(){
 
     }
