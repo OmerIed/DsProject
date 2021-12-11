821c1cdfa621254e9d87c0d3a02356fc630f5aeb
Ben Van Oost <ben.van-oost@campus.technion.ac.il>
2021-12-11 16:43:52 +0200

    update graphedge

--- src/GraphEdge.java
+++ src/GraphEdge.java
@@ -9,13 +9,52 @@ public class GraphEdge {
     {
         this.from = from;
         this.to = to;
-        fromNext = from.getInLast().
+        fromPrev = from.getInLast();
+        from.getOutLast().(this);
         this.fromNext = null;
         this.fromPrev = null;
         this.toNext = null;
     }
 
-    public
+    public void setFromNext(GraphEdge fromNext) {
+        this.fromNext = fromNext;
+    }
+
+    public GraphEdge getFromNext() {
+        return fromNext;
+    }
+
+    public GraphEdge getFromPrev() {
+        return fromPrev;
+    }
+
+    public GraphEdge getToNext() {
+        return toNext;
+    }
+
+    public GraphEdge getToPrev() {
+        return toPrev;
+    }
+
+    public void setFrom(GraphNode from) {
+        this.from = from;
+    }
+
+    public void setFromPrev(GraphEdge fromPrev) {
+        this.fromPrev = fromPrev;
+    }
+
+    public void setTo(GraphNode to) {
+        this.to = to;
+    }
+
+    public void setToNext(GraphEdge toNext) {
+        this.toNext = toNext;
+    }
+
+    public void setToPrev(GraphEdge toPrev) {
+        this.toPrev = toPrev;
+    }
 
     public GraphNode getFrom() {
         return from;
